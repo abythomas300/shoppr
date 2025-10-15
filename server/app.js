a9d6665dcc3cb5b7b3170035775706a2b8ab96f7
abythomas300 <abythomas300@gmail.com>
2025-10-15 23:06:03 +0530

    test: sample post route

--- server/app.js
+++ server/app.js
@@ -2,14 +2,28 @@ const express = require('express')
 const dotenv = require('dotenv')
 const dbConfiguration = require('./config/db')
 
+
+// package configurations
 const app = express()
 dotenv.config({path: '.env'})
 
+
+// general middlewares
+app.use(express.json())
+
+
+// route handlers
 app.get('/', (req, res)=>{
     res.send("Test Success ✅")
-    console.log(`Request received for ${req.url} using ${req.method} method`)
+    
 })
 
+app.post('/', (req, res)=>{
+    res.send(req.body)
+})
+
+
+// start server
 app.listen(process.env.PORT, async ()=>{
     await dbConfiguration.connectDB() 
     console.log(`Server is running on port ${process.env.PORT}`)
