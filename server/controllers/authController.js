58f5b65171a780e5e74d94b183c38dfcc4926b8c
abythomas300 <abythomas300@gmail.com>
2025-10-18 02:07:02 +0530

    feat(auth): add user registration functionality

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,9 +1,25 @@
+const userModel = require('../models/User')
+const bcrypt = require('bcrypt')
+
 function displaySignupPage(req, res) {
     res.send("Shoppr Signup Page ✅")
 }
 
-function registerUser(req, res) {
-    res.send("This function will handle user registration ✅")
+async function registerUser(req, res) {
+    try{
+        const rawUserInfo = req.body 
+        // generating password hash
+        const hash = await bcrypt.hash(rawUserInfo.password, 10)
+        const userInfoWithHashedPassword = {...rawUserInfo, password:hash}  // replacing password field with the password hash
+        const data = new userModel(userInfoWithHashedPassword)
+        await data.save()
+        console.log("User info saved to DB successfully")
+        res.send("Registration Successful")
+    }
+    catch(error){
+        console.log("User registration failed, reason: ", error)
+        res.send("Failed to register user")
+    }
 }
 
 function displayLoginPage(req, res) {
