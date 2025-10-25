1a032a1c591ab69fcf9fd718e6a74d2dcb6f2bbd
abythomas300 <abythomas300@gmail.com>
2025-10-25 15:53:52 +0530

    feat(models): add Review model

---
+++ server/models/Review.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose")
+
+const reviewSchema = new mongoose.Schema({
+    productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 5,
+        required: true
+    },
+    comment: {
+        type: String
+    },
+}, {
+    timestamps: true
+})
+
+const Review = mongoose.model('reviews', reviewSchema)
+
+module.exports = Review
