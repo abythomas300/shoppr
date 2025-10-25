1af4d23c19a8ef7c45bcdac608f5503a30505c4b
abythomas300 <abythomas300@gmail.com>
2025-10-25 15:42:20 +0530

    feat(models): add Order model

---
+++ server/models/Order.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose")
+
+const orderSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    orderItems: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product'
+    }],
+    shippingAddress: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    paymentId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Payment'
+    },
+    status: {
+        type: String,
+        enum: ['Pending', 'Processing', 'Shipped', 'Delivered', 'Cancelled'],
+        default: 'Pending'
+    },
+    totalAmount: {
+        type: Number,
+        required: true
+    },
+    placedAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+const Order = mongoose.model('orders', orderSchema)
+
+module.exports = Order
