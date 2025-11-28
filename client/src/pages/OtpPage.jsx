04d3187bbdb2e8d218bbf8a78f7d0aedce04f7de
abythomas300 <abythomas300@gmail.com>
2025-11-29 00:10:26 +0530

    feat(pages): add OTP input page

---
+++ client/src/pages/OtpPage.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/layout/Header";
+import Footer from "../components/layout/Footer";
+import OTPForm from "../components/layout/OTPForm";
+
+function OTPPage() {
+    return(
+        <>
+        <Header />
+        <OTPForm />
+        <Footer />
+        </>        
+    )
+}
+
+export default OTPPage
