136b3c037d026a0bf52afde7794e6270b6462084
abythomas300 <abythomas300@gmail.com>
2025-11-29 00:14:53 +0530

    refactor(routes): remove unwanted route handlers

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,11 +4,9 @@ const authController = require('../controllers/authController')
 
 
 // route handlers
-router.get('/signup', authController.displaySignupPage)
 router.post('/signup', authController.registerUser)
-router.get('/login', authController.displayLoginPage)
 router.post('/login', authController.loginUser)
-
+router.post('/verify-otp', authController.verifyOTP)
 
 // exporting module
 module.exports = router
