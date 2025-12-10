97a2c4f1affc1bebfc987ccfd7e96d3f7a1b22d9
abythomas300 <abythomas300@gmail.com>
2025-12-10 23:33:07 +0530

    feat(redux): provide redux global store access to entire application

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,15 @@
 import {createRoot} from 'react-dom/client'
 import App from './app'
 import {BrowserRouter} from 'react-router-dom'
+import {Provider} from 'react-redux'
+import store from './store'
 
 const root = createRoot(document.querySelector("#root"))
 
 root.render(
-    <BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
         <App/>
-    </BrowserRouter>
+        </BrowserRouter>
+    </Provider>
 )
