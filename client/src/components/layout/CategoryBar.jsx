86b3dedb675b0616bf300c56b8e86a01d6c54a0b
abythomas300 <abythomas300@gmail.com>
2025-12-10 23:43:10 +0530

    fix(layout/CategoryBar): remove unwanted event handlers

--- client/src/components/layout/CategoryBar.jsx
+++ client/src/components/layout/CategoryBar.jsx
@@ -15,14 +15,13 @@ const categories = [
   { id: 5, name: "Skincare", icon: <Sparkles className="w-6 h-6" /> },
 ];
 
-const CategoryBar = ({ onCategorySelect }) => {
+const CategoryBar = () => {
   return (
     <div className="w-full bg-base-300 shadow-sm">
       <div className="flex md:justify-center gap-4 overflow-x-auto px-4 py-2 sm:gap-6 scrollbar-hide">
         {categories.map((category) => (
           <button
             key={category.id}
-            onClick={() => onCategorySelect?.(category.name)}
             className="btn flex flex-col items-center justify-center bg-base-100 text-base-content hover:bg-primary hover:text-primary-content rounded-xl p-2 w-24 h-24 transition-all duration-300 shadow-md hover:shadow-lg active:scale-95 "
           >
             <span>{category.icon}</span>
