35a324a87428dd4602cbfec1760273e5fbb0ad81
abythomas300 <abythomas300@gmail.com>
2025-12-12 21:45:55 +0530

    feat(redux): add products, orders, cart, wishlist and user reducer functions to global store

--- client/src/store.js
+++ client/src/store.js
@@ -1,9 +1,17 @@
 import {configureStore} from '@reduxjs/toolkit'
 import productReducer from './features/products/productsSlice'
+import cartReducer from './features/cart/cartSlice'
+import orderReducer from './features/orders/ordersSlice'
+import wishlistReducer from './features/wishlist/wishlistSlice'
+import userReducer from './features/user/userSlice'
 
 const store = configureStore({
     reducer: {
-        products: productReducer
+        products: productReducer,
+        orders: orderReducer,
+        cart: cartReducer,
+        wishlist: wishlistReducer,
+        user: userReducer
     }
 })
 
