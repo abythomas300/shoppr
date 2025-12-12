6210bbb94252c1bbd5978a5f979e0af412d8ce37
abythomas300 <abythomas300@gmail.com>
2025-12-12 22:07:12 +0530

    feat(pages): get data from global store

--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -3,9 +3,12 @@ import UserIconSolid from "../components/common/UserIconSolid"
 import Header from "../components/layout/Header"
 import Footer from '../components/layout/Footer'
 import PageHeader from "../components/layout/PageHeader"
-
+import {useSelector } from "react-redux";
 
 function UserProfile() {
+
+    const details = useSelector(state => state.user.details)
+    
     return(
         <>
             <Header/>
@@ -14,7 +17,10 @@ function UserProfile() {
             <div className="max-w-6xl mx-auto px-4 py-3">
                 {/* profile page wrapper */}
                 <div className=" border border-base-300 p-4">
-                    <form>
+                    {
+                        details.map((detail)=>{
+                            return(
+                                <form>
                         {/* access credentials section*/}
                         <div className="flex flex-col p-4">
                             <div className="flex flex-row">
@@ -22,9 +28,9 @@ function UserProfile() {
                                     <legend className="fieldset-legend">Access Information</legend>
                                     <div className="flex gap-4 justify-baseline">
                                         <span className="text-lg">Firstname</span>
-                                        <input type="text" className="input" placeholder="" name="firstname" value="old firstname"/>
+                                        <input type="text" className="input" placeholder="" name="firstname" value={detail.firstName}/>
                                         <span className="text-lg">Lastname</span>
-                                        <input type="text" className="input" placeholder="" name="lastname" value="old lastname"/>
+                                        <input type="text" className="input" placeholder="" name="lastname" value={detail.lastName}/>
                                     </div>
                                 </fieldset>
                             </div>
@@ -36,15 +42,13 @@ function UserProfile() {
                                     <legend className="fieldset-legend">Personal Information</legend>
                                     <div className="flex gap-4">
                                         <span className="text-lg">Username</span>
-                                        <input type="text" className="input" placeholder="" name="username" value="old username"/>
+                                        <input type="text" className="input" placeholder="" name="username" value={detail.username}/>
                                         <span className="text-lg">Password</span>
-                                        <input type="password" className="input" placeholder="123456" name="password"/>
+                                        <input type="password" className="input" placeholder="123456" name={detail.password}/>
                                     </div>
-                                    <div className="flex flex-col gap-4">
+                                    <div className="flex mt-2 gap-4">
                                         <span className="text-lg">Email</span>
-                                        <input type="text" className="input" placeholder="" name="username" value="old email"/>
-                                        <span className="text-lg">Password</span>
-                                        <input type="password" className="input" placeholder="" name="password" value="old password"/>
+                                        <input type="text" className="input" placeholder="" name="username" value={detail.email}/>
                                     </div>
                                 </fieldset>
                             </div>
@@ -54,19 +58,19 @@ function UserProfile() {
                                 <fieldset className="fieldset text-lg border p-4 border-primary rounded-xl">
                                     <legend className="fieldset-legend">Address Information</legend>
                                     <span className="text-lg">Address Label</span>
-                                    <input type="text" className="input" placeholder="home" name="addressLabel" value="old lastname"/>
-                                    <span className="text-lg">Housename</span>
-                                    <input type="text" className="input" placeholder="" name="housename" value="old housename"/>
-                                    <span className="text-lg">Street </span>
-                                    <input type="text" className="input" placeholder="" name="street" value="street name"/>
+                                    <input type="text" className="input" placeholder="home" name="addressLabel" value={detail.address[0].addressLabel}/>
+                                    <span className="text-lg">House Name</span>
+                                    <input type="text" className="input" placeholder="" name="housename" value={detail.address[0].housename}/>
+                                    <span className="text-lg">Street</span>
+                                    <input type="text" className="input" placeholder="" name="street" value={detail.address[0].street}/>
                                     <span className="text-lg">City</span>
-                                    <input type="text" className="input" placeholder="" name="city" value="city name"/>
+                                    <input type="text" className="input" placeholder="" name="city" value={detail.address[0].city}/>
                                     <span className="text-lg">State</span>
-                                    <input type="text" className="input" placeholder="" name="state" value="state name"/>
+                                    <input type="text" className="input" placeholder="" name="state" value={detail.address[0].state}/>
                                     <span className="text-lg">Country</span>
-                                    <input type="text" className="input" placeholder="" name="state" value="country"/>
+                                    <input type="text" className="input" placeholder="" name="state" value={detail.address[0].country}/>
                                     <span className="text-lg">Pincode</span>
-                                    <input type="number" className="input" placeholder="" name="pincode" value="old pincode"/>
+                                    <input type="number" className="input" placeholder="" name="pincode" value={detail.address[0].pincode}/>
                                 </fieldset>
                             </div>
                         {/* preferences section */}
@@ -93,6 +97,9 @@ function UserProfile() {
                             <button className="col-span-12 md:col-span-6 w-full btn bg-primary btn-md lg:btn-lg xl:btn-xl">Edit Details</button>
                         </div>
                     </form>
+                            )
+                        })
+                    }
                 </div>
             </div>
             <Footer/>
