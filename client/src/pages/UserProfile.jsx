88da9726e97508c4db5c12ab6fae6ac79ca6289b
abythomas300 <abythomas300@gmail.com>
2025-12-19 20:37:28 +0530

    feat(pages): show available user address in profile page

--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -3,12 +3,64 @@ import UserIconSolid from "../components/common/UserIconSolid"
 import Header from "../components/layout/Header"
 import Footer from '../components/layout/Footer'
 import PageHeader from "../components/layout/PageHeader"
-import {useSelector } from "react-redux";
+import {useSelector} from "react-redux";
+
+function AddressComponent() {
+    const detail = useSelector(state => state.auth.userDetails)
+    const address = detail.address
+    if(address && address.length > 0) {
+        // If there is 1 or more address available
+        return(
+            <>
+                {
+                    address.map((info, key)=>(
+                        <>
+                            <span className="hidden" key={key}></span>
+                            <span className="text-lg">Address Label</span>
+                            <input type="text" className="input" placeholder="home" name="addressLabel" value={info.addressLabel}/>
+                            <span className="text-lg">House Name</span>
+                            <input type="text" className="input" placeholder="" name="housename" value={info.housename}/>
+                            <span className="text-lg">Street</span>
+                            <input type="text" className="input" placeholder="" name="street" value={info.street}/>
+                            <span className="text-lg">City</span>
+                            <input type="text" className="input" placeholder="" name="city" value={info.city}/>
+                            <span className="text-lg">State</span>
+                            <input type="text" className="input" placeholder="" name="state" value={info.state}/>
+                            <span className="text-lg">Country</span>
+                            <input type="text" className="input" placeholder="" name="country" value={info.country}/>
+                            <span className="text-lg">Pincode</span>
+                            <input type="number" className="input" placeholder="" name="pincode" value={info.pincode}/>
+                        </>
+                    ))
+                }
+            </>
+        )
+    } else {
+            return(
+                <>
+                    <span className="text-lg">Address Label</span>
+                    <input type="text" className="input" placeholder="not set" name="addressLabel"/>
+                    <span className="text-lg">House Name</span>
+                    <input type="text" className="input" placeholder="not set" name="housename"/>
+                    <span className="text-lg">Street</span>
+                    <input type="text" className="input" placeholder="not set" name="street"/>
+                    <span className="text-lg">City</span>
+                    <input type="text" className="input" placeholder="not set" name="city"/>
+                    <span className="text-lg">State</span>
+                    <input type="text" className="input" placeholder="not set" name="state"/>
+                    <span className="text-lg">Country</span>
+                    <input type="text" className="input" placeholder="not set" name="state"/>
+                    <span className="text-lg">Pincode</span>
+                    <input type="number" className="input" placeholder="not set" name="pincode"/>
+                </>
+            )
+    }
+}
 
 function UserProfile() {
 
-    const details = useSelector(state => state.user.details)
-    
+    const detail = useSelector(state => state.auth.userDetails)
+    console.log("user details from UserProfile.jsx: ", detail) // profile page
     return(
         <>
             <Header/>
@@ -17,10 +69,8 @@ function UserProfile() {
             <div className="max-w-6xl mx-auto px-4 py-3">
                 {/* profile page wrapper */}
                 <div className=" border border-base-300 p-4">
-                    {
-                        details.map((detail)=>{
-                            return(
-                                <form>
+
+                    <form>
                         {/* access credentials section*/}
                         <div className="flex flex-col p-4">
                             <div className="flex flex-row">
@@ -43,10 +93,6 @@ function UserProfile() {
                                     <div className="flex gap-4">
                                         <span className="text-lg">Username</span>
                                         <input type="text" className="input" placeholder="" name="username" value={detail.username}/>
-                                        <span className="text-lg">Password</span>
-                                        <input type="password" className="input" placeholder="123456" name={detail.password}/>
-                                    </div>
-                                    <div className="flex mt-2 gap-4">
                                         <span className="text-lg">Email</span>
                                         <input type="text" className="input" placeholder="" name="username" value={detail.email}/>
                                     </div>
@@ -54,26 +100,14 @@ function UserProfile() {
                             </div>
                         </div>
                         {/* address section section*/}
-                            <div className="p-4">
-                                <fieldset className="fieldset text-lg border p-4 border-primary rounded-xl">
-                                    <legend className="fieldset-legend">Address Information</legend>
-                                    <span className="text-lg">Address Label</span>
-                                    <input type="text" className="input" placeholder="home" name="addressLabel" value={detail.address[0].addressLabel}/>
-                                    <span className="text-lg">House Name</span>
-                                    <input type="text" className="input" placeholder="" name="housename" value={detail.address[0].housename}/>
-                                    <span className="text-lg">Street</span>
-                                    <input type="text" className="input" placeholder="" name="street" value={detail.address[0].street}/>
-                                    <span className="text-lg">City</span>
-                                    <input type="text" className="input" placeholder="" name="city" value={detail.address[0].city}/>
-                                    <span className="text-lg">State</span>
-                                    <input type="text" className="input" placeholder="" name="state" value={detail.address[0].state}/>
-                                    <span className="text-lg">Country</span>
-                                    <input type="text" className="input" placeholder="" name="state" value={detail.address[0].country}/>
-                                    <span className="text-lg">Pincode</span>
-                                    <input type="number" className="input" placeholder="" name="pincode" value={detail.address[0].pincode}/>
-                                </fieldset>
-                            </div>
+                        <div className="p-4">
+                            <fieldset className="fieldset text-lg border p-4 border-primary rounded-xl">
+                                <legend className="fieldset-legend">Address Information</legend>
+                                <AddressComponent/>
+                            </fieldset>
+                        </div>
                         {/* preferences section */}
+                        
                         <div>
                             <div className="p-4">
                                 <fieldset className="fieldset text-lg border p-4 border-primary rounded-xl">
@@ -97,9 +131,6 @@ function UserProfile() {
                             <button className="col-span-12 md:col-span-6 w-full btn bg-primary btn-md lg:btn-lg xl:btn-xl">Edit Details</button>
                         </div>
                     </form>
-                            )
-                        })
-                    }
                 </div>
             </div>
             <Footer/>
