d6eea50c3676382a9947c48c0d6bf234f1a8ed14
abythomas300 <abythomas300@gmail.com>
2025-12-19 20:35:22 +0530

    feat(components/header): add hyperlink to login page

--- client/src/components/layout/HeaderProfileSection.jsx
+++ client/src/components/layout/HeaderProfileSection.jsx
@@ -1,6 +1,11 @@
 import {Link} from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 function HeaderProfileSection(props) {
+
+    const isLoggedIn = useSelector(state => state.auth.isLoggedIn)
+    const userDetails = useSelector(state => state.auth.userDetails)
+
     if(props.isLoggedIn){
         return (
             <>
@@ -9,12 +14,13 @@ function HeaderProfileSection(props) {
                             <div tabIndex={0} role="button" className="btn btn-circle avatar ">
                                 <div className="w-10 rounded-full">
                                     <img 
-                                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"  // dynamic
+                                    src="https://images.unsplash.com/photo-1641423914598-288fee6cecf2?q=80&w=880&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                                     alt="user-profile-picture" />
                                 </div>
                             </div>
                             <div className="indicator">
-                                <span className='text-xl italic'>Alice</span>
+                                { isLoggedIn ? <span>{userDetails.firstName}</span> : <span>User</span> }
+                                
                             </div>
                         </div>
                     </Link>
@@ -36,7 +42,9 @@ function HeaderProfileSection(props) {
         <>
                 {/* {Proile Icon and Login Prompt - When logged out} */}
                 <div className="flex">
-                    <button className='btn btn-ghost bg-secondary text-secondary-content'>Login/Signup</button>
+                    <Link to='/login'>
+                        <button className='btn btn-ghost bg-secondary text-secondary-content'>Login/Signup</button>
+                    </Link>
                 </div>
         </>
     )
