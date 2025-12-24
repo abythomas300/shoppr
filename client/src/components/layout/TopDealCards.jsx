43dfc9e1469e1d6de37fecc6dbc611051d806dd9
abythomas300 <abythomas300@gmail.com>
2025-12-24 12:09:25 +0530

    feat(pages/TopDealCards): add API call for getting products

--- client/src/components/layout/TopDealCards.jsx
+++ client/src/components/layout/TopDealCards.jsx
@@ -1,20 +1,23 @@
-import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { addToCart } from "../../features/cart/cartSlice";
+import axios from 'axios';
+import { setProduct } from "../../features/products/productsSlice";
 
 function TopDealsCards() {
 
-        const products = useSelector(state => state.products.items)
+        let products = useSelector(state => state.products.items)
         const dispatch = useDispatch()
 
         const handleAddToCartButtonClick = (id)=>{
           // get selected product from global state
+          console.log("Target id:", id) // for test
           const selectedProduct = products.filter((product)=>{
-            if(product.id === id)
+            if(product._id === id) 
               return product
           })
           // dispatch addToCart() from cartSlice
           if(selectedProduct)
+            console.log("SELECTED PRODUCT:", selectedProduct) // for test
             dispatch(addToCart(selectedProduct[0]))
         }
 
@@ -23,12 +26,32 @@ function TopDealsCards() {
           console.log("buy now - button click test")
         }
         
+        // API call for getting products
+        const getProducts = async () => {
+          try {
+            const response = await axios.get('http://localhost:3000/product/')
+            if(response.data.success) {
+              products = response.data.products
+              console.log("API call success, response: ", products) // for test
+              // setting products data fetched from API to productsSlice
+              dispatch(setProduct(products))
+              console.log("new products dispatched to products global state!") // for test
+            }
+          } catch(error) {
+            console.log("Error in getting products, reason: ", error)
+          }
+        }
+
+        // Do API call for getting products only if the productsSlice state is null (using useState is causing re-render loop)
+        if(products === null)
+          getProducts()
+
         return (
           <section className="px-6 py-10 bg-base-200"> 
             <h2 className="text-2xl font-bold text-start mb-8">Recently Added</h2>
       
             <div className="grid gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 ">
-              {products.length > 0 ? (
+              {(products && products.length > 0) ? (
                 products.map((product) => (
                   <div
                     key={product.id}
@@ -54,7 +77,7 @@ function TopDealsCards() {
                         <span className="mr-1">₹</span>{product.price}
                       </p>
                       <div className="card-actions mt-2">
-                        <button onClick={ ()=>{handleAddToCartButtonClick(product.id)} } className="btn btn-primary btn-sm text-sm ">
+                        <button onClick={ ()=>{handleAddToCartButtonClick(product._id)} } className="btn btn-primary btn-sm text-sm ">
                           Add to Cart
                         </button>
                         <button onClick={handleBuyNowButtonClick} className="btn btn-primary btn-sm text-sm">
