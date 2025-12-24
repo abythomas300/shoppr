adc0d36efc047cef9dfd4d0a42781c2ba4955e15
abythomas300 <abythomas300@gmail.com>
2025-12-24 12:15:48 +0530

    feat(redux/slice): add setProduct() reducer

--- client/src/features/products/productsSlice.js
+++ client/src/features/products/productsSlice.js
@@ -1,17 +1,20 @@
 import {createSlice} from "@reduxjs/toolkit"
-import products from './product-db-test'
+// import products from './product-db-test'
 
 const productSlice = createSlice({
     name: 'productSlice',
     initialState: {
-        items: products
+        items: null
     },
     reducers: {
+        setProduct(state, action) {
+            state.items = action.payload
+        },
         addProduct(state, action) {
             state.items.push(action.payload)
         }
     }
 })
 
-export const {addProduct} = productSlice.actions
+export const {setProduct, addProduct} = productSlice.actions
 export default productSlice.reducer
