1b155682ddb87239e6e0648463d57280cc665070
abythomas300 <abythomas300@gmail.com>
2026-01-20 22:50:11 +0530

    feat(config): add connection management events in socket config

--- client/src/config/socket.js
+++ client/src/config/socket.js
@@ -4,4 +4,12 @@ const socket = io('http://localhost:3000', {
     withCredentials: true
 })
 
+socket.on('connect', ()=>{
+    console.log("Client connected to socket.io server")
+})
+
+socket.on('connect_error', ()=>{
+    console.log("Connection lost, check your internet connection and try again.")
+})
+
 export default socket
