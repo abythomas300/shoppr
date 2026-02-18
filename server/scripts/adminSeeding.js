0547fbc8ef194e536efa3ab1145c265d9527437d
abythomas300 <abythomas300@gmail.com>
2026-02-18 14:02:48 +0530

    fix(test/seeding): add password hashing to admin seeding

--- server/scripts/adminSeeding.js
+++ server/scripts/adminSeeding.js
@@ -1,6 +1,7 @@
 const userModel = require('../models/User')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
+const bcrypt = require('bcrypt')
 
 dotenv.config({path: '../.env'})
 
@@ -10,28 +11,29 @@ const data = {
     email: 'admin.shoppr@gmail.com',
     password: 'admin',
     firstName: 'Admin',
+    lastName: '',
     role: 'admin',
-    lastName: 'last-name',
-    phone: '+919999999999',
-    address: [{}]
+    phone: '+919999999999'
 }
 
 // seeding function
-async function userSeeding(data) {
-
+async function adminSeeding(data) {
     try{
         await mongoose.connect(process.env.MONGO_URI)
         console.log("DB Connection successful")
-        const newUser = new userModel(data)  // creating a model instance
-        const result = await newUser.save()
-        console.log('Admin data inserted successfully!')
-        console.log('returns: ', result)
+        // generate password hash
+        const hash = await bcrypt.hash(data.password, 10)
+        const adminData = {...data, password:hash}
+        const newAdmin = await userModel.create(adminData)
+        if(!newAdmin)
+            throw new Error("admin seed failed, invalid data format")
+        console.log("Admin seeded successfully")
         process.exit(0)
     }catch(error){
-        console.log("Cannot insert data, reason: ", error)
+        console.log("Cannot seed admin, reason: ", error)
         process.exit(1)
     }
 
 }
 
-userSeeding(data)
+adminSeeding(data)
